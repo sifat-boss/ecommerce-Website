cad3bd484c42a86171b68da100eeed53c841b5bc
coder sifat <sifatcreator999@gmail.com>
2023-03-31 12:59:55 +0600

    10th commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const bar = document.getElementById('bar');
-const closes = document.getElementById('close');
-const nav = document.getElementById('navbar');
-if(bar){
-    bar.addEventListener('click',()=>{
-        nav.classList.add('active');
-});
+const bar = document.getElementById("bar");
+const closes = document.getElementById("close");
+const nav = document.getElementById("navbar");
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
-if(closes){
-    closes.addEventListener('click',()=>{
-        nav.classList.remove('active');
-});
+if (closes) {
+  closes.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 }
